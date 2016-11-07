51b983f19e29fb8bd9c4729c35f8fad68231515e
huoshan12345 <89009143@qq.com>
2016-11-07 23:53:36 +0800

    add a string to enum method

--- src/FxUtility/Extensions/EnumExtensions.cs
+++ src/FxUtility/Extensions/EnumExtensions.cs
@@ -9,5 +9,16 @@ namespace FxUtility.Extensions
         {
             return Convert.ToInt32(@enum);
         }
+
+        public static T ToEnum<T>(this string value, T defaultValue = default(T)) where T : struct, IConvertible
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+
+            T result;
+            return Enum.TryParse(value, true, out result) ? result : defaultValue;
+        }
     }
 }
