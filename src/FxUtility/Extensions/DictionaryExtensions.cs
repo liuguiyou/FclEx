e45b9b7ba46f63c921b9444a2e472dbc545ceb00
huoshan12345 <89009143@qq.com>
2016-11-12 18:37:33 +0800

    add some dic extensions

--- src/FxUtility/Extensions/DictionaryExtensions.cs
+++ src/FxUtility/Extensions/DictionaryExtensions.cs
@@ -42,11 +42,32 @@ namespace FxUtility.Extensions
             }
         }
 
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> dic, IEnumerable<TValue> items, Func<TValue, TKey> func)
+        {
+            foreach (var item in items)
+            {
+                var key = func(item);
+                if (!dic.ContainsKey(key))
+                {
+                    dic.Add(key, item);
+                }
+            }
+        }
+
         public static void AddOrUpdateRange<TKey, TValue>(this IDictionary<TKey, TValue> dic, IEnumerable<KeyValuePair<TKey, TValue>> pairs)
         {
             foreach (var pair in pairs)
             {
                 dic[pair.Key] = pair.Value;
+            }
+        }
+
+        public static void AddOrUpdateRange<TKey, TValue>(this IDictionary<TKey, TValue> dic, IEnumerable<TValue> items, Func<TValue, TKey> func)
+        {
+            foreach (var item in items)
+            {
+                var key = func(item);
+                dic[key] = item;
             }
         }
 
