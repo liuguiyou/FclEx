574d239c54ec00b224d3d6ab2f4fb78bf307320b
huoshan12345 <89009143@qq.com>
2016-11-26 23:10:55 +0800

    add some Queryable Extensions

---
+++ src/FxUtility/Extensions/QueryableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FxUtility.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, int, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, int, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
