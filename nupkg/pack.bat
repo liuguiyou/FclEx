c9e4535e917d481e6f5e70e8d100d2866b4a0426
huoshan12345 <89009143@qq.com>
2017-03-12 00:57:51 +0800

    update to vs2017

--- nupkg/pack.bat
+++ nupkg/pack.bat
@@ -2,7 +2,7 @@ REM "..\tools\gitlink\GitLink.exe" ..\ -u https://github.com/huoshan12345/FclEx 
 
 @ECHO OFF
 SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty): 
-dotnet pack "..\src\FclEx" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
-dotnet pack "..\src\FclEx.Component" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
-dotnet pack "..\src\FclEx.Json" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
+dotnet pack "..\src\FclEx" -c "Release" -o ..\..\nupkg\ --version-suffix "%VERSION_SUFFIX%"
+dotnet pack "..\src\FclEx.Component" -c "Release" -o ..\..\nupkg\ --version-suffix "%VERSION_SUFFIX%"
+dotnet pack "..\src\FclEx.Json" -c "Release" -o ..\..\nupkg\ --version-suffix "%VERSION_SUFFIX%"
 pause
--- src/FclEx/Properties/AssemblyInfo.cs
+++ src/FclEx/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FxUtility")]
+[assembly: AssemblyProduct("FclEx")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
