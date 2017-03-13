7f3d0bb2bb65572ece31dd822bdbeb83113fe98a
huoshan12345 <89009143@qq.com>
2017-03-13 14:21:48 +0800

    add a empty logger

---
+++ src/FclEx.Component/Logger/EmptyLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace FclEx.Logger
+{
+    public class EmptyLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
+            Func<TState, Exception, string> formatter)
+        {
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return Disposable.Singleton;
+        }
+    }
+
+    internal class Disposable : IDisposable
+    {
+        public static Disposable Singleton { get; } = new Disposable();
+
+        public void Dispose() { }
+    }
+}
