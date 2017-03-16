004c1213edab75772e8e18990603e5b4604454bd
huoshan12345 <89009143@qq.com>
2017-03-16 15:17:19 +0800

    add some task extensions

--- src/FclEx/Extensions/TaskExtensions.cs
+++ src/FclEx/Extensions/TaskExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FclEx.Extensions
 {
@@ -9,5 +10,15 @@ namespace FclEx.Extensions
         /// </summary>
         /// <param name="task"></param>
         public static void Forget(this Task task) { }
+
+        public static Task WhenAll(this IEnumerable<Task> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
     }
 }
