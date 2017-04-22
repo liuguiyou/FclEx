3496a062379dedb120a1138f5f3445e9586f0972
huoshan12345 <89009143@qq.com>
2017-04-22 16:28:37 +0800

    use the powershell script which from abp project to pack dlls

--- nupkg/pack.bat
+++
@@ -1,8 +0,0 @@
-REM "..\tools\gitlink\GitLink.exe" ..\ -u https://github.com/huoshan12345/FclEx -c release
-
-@ECHO OFF
-SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty): 
-dotnet pack "..\src\FclEx" -c "Release" -o ..\..\nupkg\ --include-symbols --version-suffix "%VERSION_SUFFIX%"
-dotnet pack "..\src\FclEx.Component" -c "Release" -o ..\..\nupkg\ --include-symbols --version-suffix "%VERSION_SUFFIX%"
-dotnet pack "..\src\FclEx.Json" -c "Release" -o ..\..\nupkg\ --include-symbols --version-suffix "%VERSION_SUFFIX%"
-pause
