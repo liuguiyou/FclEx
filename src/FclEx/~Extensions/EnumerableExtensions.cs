21087fb7dfbf3308acc86160fdb09a12a4e0ad4b
huoshan12345 <89009143@qq.com>
2017-12-19 22:42:23 +0800

    remove conflicted extensions from other libs

--- src/FclEx/~Extensions/EnumerableExtensions.cs
+++ src/FclEx/~Extensions/EnumerableExtensions.cs
@@ -2,19 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MoreLinq;
 
 namespace FclEx
 {
     public static class EnumerableExtensions
     {
-        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
-        {
-            foreach (var item in enumeration)
-            {
-                action(item);
-            }
-        }
-
         public static Task<TResult[]> ForEachAsync<T, TResult>(this IEnumerable<T> sequence, Func<T, Task<TResult>> action)
         {
             return Task.WhenAll(sequence.Select(action).ToArray());
