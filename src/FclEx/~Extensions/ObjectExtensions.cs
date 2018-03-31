fd9f0970d08a559bca7c703d94a16546c5ed3cb7
huoshan12345 <89009143@qq.com>
2018-03-31 17:17:13 +0800

    fix bugs

--- src/FclEx/~Extensions/ObjectExtensions.cs
+++ src/FclEx/~Extensions/ObjectExtensions.cs
@@ -1,16 +1,17 @@
-﻿namespace FclEx
+﻿using System.Collections.Generic;
+
+namespace FclEx
 {
     public static class ObjectExtensions
     {
-        public static bool IsDefault<T>(this T obj) => obj.Equals(default(T));
+        public static bool IsDefault<T>(this T obj) => EqualityComparer<T>.Default.Equals(obj, default);
 
         public static bool IsNull<T>(this T obj) => obj == null;
 
         public static bool IsNotNull<T>(this T obj) => obj != null;
 
-        public static bool IsNullOrDefault<T>(this T obj) => obj == null || obj.Equals(default(T));
-
-        public static T CastTo<T>(this object obj) => (T)obj;
+        public static TDestination CastTo<TSource, TDestination>(this TSource obj) where TSource : class
+            => (TDestination)(object)obj;
 
         public static string SafeToString<T>(this T obj) => obj == null ? string.Empty : obj.ToString();
     }
