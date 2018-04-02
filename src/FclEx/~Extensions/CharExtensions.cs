b317f9871028ceb0ebf50c08bcf8326c6ff9c45d
huoshan12345 <89009143@qq.com>
2018-04-02 15:40:39 +0800

    add some extensions for char

---
+++ src/FclEx/~Extensions/CharExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FclEx
+{
+    public static class CharExtensions
+    {
+        public static char ToUpper(this char c, CultureInfo culture) => char.ToUpper(c, culture);
+        public static char ToUpper(this char c) => char.ToUpper(c);
+        public static char ToUpperInvariant(this char c) => char.ToUpperInvariant(c);
+        public static char ToLower(this char c, CultureInfo culture) => char.ToLower(c, culture);
+        public static char ToLower(this char c) => char.ToLower(c);
+        public static char ToLowerInvariant(this char c) => char.ToLowerInvariant(c);
+    }
+}
