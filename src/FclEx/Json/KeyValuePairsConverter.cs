a877e91eb53509b33bfaa3e5013563aa3ca5b9d5
李经 <huoshan12345@hotmail.com>
2018-06-29 02:49:00 +0800

    fix bugs

--- src/FclEx/Json/KeyValuePairsConverter.cs
+++ src/FclEx/Json/KeyValuePairsConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
@@ -10,22 +11,11 @@ namespace FclEx.Json
 {
     public class KeyValuePairsConverter : JsonConverter
     {
-        private static readonly MethodInfo _toArrayMethod = typeof(Enumerable).GetMethod(nameof(Enumerable.ToArray));
-        private static MethodInfo _castMethod = typeof(Enumerable).GetMethod(nameof(Enumerable.Cast));
+        public override bool CanRead { get; } = true;
+        public override bool CanWrite { get; } = false;
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            var list = value as IEnumerable<KeyValuePair<string, object>>;
-            writer.WriteStartArray();
-            foreach (var item in list)
-            {
-                writer.WriteStartObject();
-                writer.WritePropertyName(item.Key);
-                writer.WriteValue(item.Value);
-                writer.WriteEndObject();
-            }
-            writer.WriteEndArray();
-        }
+        private static readonly MethodInfo _toArrayMethod = typeof(Enumerable).GetMethod(nameof(Enumerable.ToArray));
+
 
         public override bool CanConvert(Type objectType)
         {
@@ -47,30 +37,36 @@ namespace FclEx.Json
             var colType = objectType.GetGenericTypeDefinition();
             if (objectType.IsInstanceOfType(list)) return list;
 
-            if (colType.IsAbstract) 
+            if (!colType.IsAbstract)
             {
+                if (colType == typeof(ReadOnlyCollection<>)) return objectType.CreateObject(list);
+
                 var ctor = colType.GetConstructor(new[] { typeof(IEnumerable<>).MakeGenericType(eleType) });
                 if (ctor != null) return ctor.Invoke(new object[] { list });
+
                 if (colType.IsInheritedFromGenericType(typeof(ICollection<>)))
                 {
                     var obj = objectType.CreateObject();
                     var addMethod = objectType.GetMethod(nameof(ICollection<object>.Add));
                     foreach (var item in list)
                     {
-                        addMethod.Invoke(obj, new []{ item });
+                        addMethod.Invoke(obj, new[] { item });
                     }
+                    return obj;
                 }
             }
-
-            // try to cast
+            // just cast
             return list;
+        }
 
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType == JsonToken.Null)
-                return null;
+            if (reader.TokenType == JsonToken.Null) return null;
 
             var kvType = objectType.GetAnyElementType();
             var keyType = kvType.GenericTypeArguments[0];
@@ -79,20 +75,19 @@ namespace FclEx.Json
             var list = typeof(List<>).MakeGenericType(kvType).CreateObject().CastTo<IList>();
             var pairCtor = kvType.GetConstructor(kvType.GenericTypeArguments);
 
-            var token = JToken.ReadFrom(reader);
-            if (token.Type == JTokenType.Array)
-            {
+            Func<string, object> keyFunc = keyType == typeof(string)
+                ? s => s
+                : keyType == typeof(char)
+                    ? (Func<string, object>)(s => s[0])
+                    : s => JsonConvert.DeserializeObject(s, keyType);
 
-            }
-            else if (token.Type == JTokenType.Object)
+            var token = JToken.ReadFrom(reader);
+            foreach (var t in token.ToJObject())
             {
-                foreach (var t in token.ToJObject())
-                {
-                    var key = JsonConvert.DeserializeObject(t.Key, keyType);
-                    var value = t.Value.ToObject(valueType);
-                    var pair = pairCtor.Invoke(new[] { key, value });
-                    list.Add(pair);
-                }
+                var key = keyFunc(t.Key);
+                var value = t.Value.ToObject(valueType);
+                var pair = pairCtor.Invoke(new[] { key, value });
+                list.Add(pair);
             }
             return Convert(objectType, kvType, list);
         }
--- test/FclEx.Test/Json/KeyValuePairConverterTests.cs
+++ test/FclEx.Test/Json/KeyValuePairConverterTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -14,15 +15,42 @@ namespace FclEx.Test.Json
 {
     public class KeyValuePairConverterTests
     {
-        public static Type KvRawType { get; } = typeof(KeyValuePair<,>);
-        public static IEnumerable<int> Source { get; } = Enumerable.Range(1, 100);
-        public static IEnumerable<object[]> Cases { get; } = new object[]
+        private static readonly MethodInfo _method = typeof(KeyValuePairConverterTests).GetMethod(
+            nameof(ReadTestGeneric), BindingFlags.NonPublic | BindingFlags.Static);
+
+        private static readonly Type _kvRawType = typeof(KeyValuePair<,>);
+
+        public static IEnumerable<int> Source { get; } = Enumerable.Range(1, 10);
+
+        private static string ToCharStr(int i) => i.CastTo<char>().ToString();
+
+        public static IDictionary[] Datas { get; } =
         {
             Source.ToDictionary(m => m, m => -m),
-            Source.ToDictionary(m => m, m => (-m).ToString()),
-            Source.ToDictionary(m => m.ToString(), m => -m),
-            Source.ToDictionary(m => m.ToString(), m => (-m).ToString())
-        }.Select(m => new[] { m }).ToArray();
+            Source.ToDictionary(m => m, m => ToCharStr(m + 'a' - 1)),
+            Source.ToDictionary(m => ToCharStr(m + 'A' - 1), m => m),
+            Source.ToDictionary(m => ToCharStr(m + 'A' - 1), m => ToCharStr(m + 'a' - 1)),
+            Source.ToDictionary(m => m.ToString(), m => Source),
+            Source.ToDictionary(m => m.ToString(), m => Source.ToDictionary(s => s)),
+            Source.ToDictionary(m => (m + 'A' - 1).CastTo<char>(), m => m),
+            Source.ToDictionary(m => (m + 'A' - 1).CastTo<char>(), m => (m + 'a' - 1).CastTo<char>()),
+        };
+
+        public static Func<Type, Type>[] KvToColConvertors { get; } =
+        {
+            t => t.MakeArrayType(),
+            t => typeof(IEnumerable<>).MakeGenericType(t),
+            t => typeof(ICollection<>).MakeGenericType(t),
+            t => typeof(IList<>).MakeGenericType(t),
+            t => typeof(List<>).MakeGenericType(t),
+            t => typeof(IReadOnlyCollection<>).MakeGenericType(t),
+            t => typeof(ReadOnlyCollection<>).MakeGenericType(t),
+            t => typeof(IReadOnlyList<>).MakeGenericType(t)
+        };
+
+
+        public static IEnumerable<object[]> Cases { get; } = Datas.SelectMany(m => KvToColConvertors.Select(c => (m, c)))
+            .Select(m => new object[] { m.m, m.c }).ToArray();
 
         private static void ReadTestGeneric<T, TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> raw)
             where T : IEnumerable<KeyValuePair<TKey, TValue>>
@@ -42,39 +70,17 @@ namespace FclEx.Test.Json
             Assert.Equal(dic.Count, i);
         }
 
-        private static void ReadTest(IDictionary dic, Func<Type, Type> toColType)
+        [Theory]
+        [MemberData(nameof(Cases))]
+        public void ReadTest(IDictionary dic, Func<Type, Type> toColType)
         {
             var dicType = dic.GetType();
             var keyType = dicType.GenericTypeArguments[0];
             var valueType = dicType.GenericTypeArguments[1];
-            var kvType = KvRawType.MakeGenericType(keyType, valueType);
+            var kvType = _kvRawType.MakeGenericType(keyType, valueType);
             var colType = toColType(kvType);
             _method.MakeGenericMethod(colType, keyType, valueType)
                 .Invoke(null, new object[] { dic });
-        }
-
-        private static readonly MethodInfo _method =
-            typeof(KeyValuePairConverterTests).GetMethod(nameof(ReadTestGeneric));
-
-        [Theory]
-        [MemberData(nameof(Cases))]
-        public void ReadTest_Array(IDictionary dic)
-        {
-            ReadTest(dic, t => t.MakeArrayType());
-        }
-
-        [Theory]
-        [MemberData(nameof(Cases))]
-        public void ReadTest_List(IDictionary dic)
-        {
-            ReadTest(dic, t => t.MakeArrayType());
-        }
-
-        [Theory]
-        [MemberData(nameof(Cases))]
-        public void ReadTest_Enum(IDictionary dic)
-        {
-            ReadTest(dic, t => t.MakeArrayType());
         }
     }
 }
