87f5563087b178086e18f4225da538726de27357
huoshan12345 <89009143@qq.com>
2018-08-17 01:51:22 +0800

    fix bugs

---
+++ src/FclEx/Json/ReadAsStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FclEx.Json
+{
+    public class ReadAsStringConverter : JsonConverter
+    {
+        private static readonly JsonSerializer _defaultSerializer = new JsonSerializer();
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override bool CanRead => true;
+        public override bool CanWrite => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var token = JToken.ReadFrom(reader);
+            return token.Type == JTokenType.String 
+                ? token.Value<string>()
+                : token.ToString(Formatting.None);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
